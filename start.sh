6b6456d99dc5c0b4a52f60707c153e2a6441151e
Gabriel Baez <gabrielbaez@bohiti.org>
2016-11-19 14:52:41 -0600

    run

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! -d "/var/opt/CrushFTP8_PC/users/MainUsers/crushadmin" ]; then
-echo   sudo -u abc java -jar CrushFTP.jar -a "crushadmin" "password"
+   sudo -u abc java -jar CrushFTP.jar -a "crushadmin" "password"
     echo crushadmin created
 fi
 
-echo sudo -u abc java -jar CrushFTP.jar -d
+ sudo -u abc java -jar CrushFTP.jar -d
