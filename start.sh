a75ec86a1931afceb179fed5b3c4b9d82e2a5311
Gabriel Baez <gabrielbaez@bohiti.org>
2016-11-19 13:09:02 -0600

    maybe this willl work

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! -d "/var/opt/CrushFTP8_PC/users/MainUsers/crushadmin" ]; then
-    s6-setuidgid abc java -jar CrushFTP.jar -a "crushadmin" "password"
+    sudo -u abc java -jar CrushFTP.jar -a "crushadmin" "password"
     echo crushadmin created
 fi
 
-s6-setuidgid abc java -jar CrushFTP.jar -d
+sudo -u abc java -jar CrushFTP.jar -d
