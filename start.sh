e879a8cd6fff14c79ff13ac6fea29b8949fa1797
Gabriel Baez <gabrielbaez@bohiti.org>
2016-11-27 15:12:31 -0600

    dont need this permissions yet

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ if [ ! -d "/config/CrushFTP8_PC/users/MainUsers/crushadmin" ]; then
   
     java -jar /config/CrushFTP8_PC/CrushFTP.jar -a "crushadmin" "password" \
     echo crushadmin created \
-    chown -R abc:abc \
-	/config
+  #  chown -R abc:abc \
+#	/config
 fi
  java -jar /config/CrushFTP8_PC/CrushFTP.jar -d
 
