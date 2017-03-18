859c5fe52c73c5ae4a99f48c08fa44b729e790ee
Gabriel Baez <gabrielbaez@bohiti.org>
2017-03-18 14:46:49 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
-#!/usr/bin/with-contenv bash
-#
-if [ ! -d "/config/CrushFTP8_PC/users/MainUsers/crushadmin" ]; then
-  
-    java -jar /config/CrushFTP8_PC/CrushFTP.jar -a "crushadmin" "password" \
-    echo crushadmin created \
+#!/bin/bash
+if [ ! -d "/var/opt/CrushFTP8_PC/users/MainUsers/crushadmin" ]; then
+    java -jar CrushFTP.jar -a "crushadmin" "password"
+    echo crushadmin created
 fi
- java -jar /config/CrushFTP8_PC/CrushFTP.jar -d
+
+java -jar CrushFTP.jar -d
 
 
