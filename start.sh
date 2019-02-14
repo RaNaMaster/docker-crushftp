30d85a28e6542f3a611b5c86afc7d4850e15fe71
RaNaMaster <RaNaMaster@users.noreply.github.com>
2019-02-13 18:55:33 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ ! -d "/var/opt/CrushFTP8_PC/users/MainUsers/crushadmin" ]; then
+if [ ! -d "/var/opt/CrushFTP9_PC/users/MainUsers/crushadmin" ]; then
     java -jar CrushFTP.jar -a "crushadmin" "password"
     echo crushadmin created
 fi
